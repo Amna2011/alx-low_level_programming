86cb842b858398cc78f9529039a9c7dac1fc2974
Amna2011 <amnataha2011@gmail.com>
2024-01-26 00:06:56 -0800

    mmm

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-char *cdest, *csrc;
 
-cdest = dest;
-csrc = src;
 for (i = 0; i < n; i++)
 {
-cdest[i] = csrc[i];
+dest[i] = src[i];
 }
-return (cdest);
+
+return (dest);
 }
